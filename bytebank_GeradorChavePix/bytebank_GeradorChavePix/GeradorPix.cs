d6c3398047818b243aaa435a444435fcb1a8b0c8
Giovane Damazio <giovxnedamazio@gmail.com>
2023-12-05 17:12:18 -0300

    DLL teste

--- bytebank_GeradorChavePix/bytebank_GeradorChavePix/GeradorPix.cs
+++ bytebank_GeradorChavePix/bytebank_GeradorChavePix/GeradorPix.cs
@@ -6,13 +6,27 @@ using System.Threading.Tasks;
 
 namespace bytebank_GeradorChavePix
 {
+    /// <summary>
+    ///  Pix usando Guid.
+    /// </summary>
+
     public static class GeradorPix
     {
+
+        /// <summary>
+        /// Método chave PIX
+        /// </summary>
+        /// <returns>Retorna uma chave aleatória de PIX</returns>
         public static string GetChavePix()
         {
             return Guid.NewGuid().ToString();
         }
 
+        /// <summary>
+        /// Método que retorna uma lista aleatória de chaves PIX
+        /// </summary>
+        /// <param name="numeroChaves">Quantidade de chaves que serão geradas</param>
+        /// <returns>Retorna um List<string> de chaves pix</returns>
         public static List<string> GetChavesPix(int numeroChaves)
         {
             if (numeroChaves <= 0)
