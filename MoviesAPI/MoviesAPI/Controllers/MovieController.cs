c2fc3e114ed645bcc2e3faa4f2c854a455d35f6c
Giovane Damazio <giovxnedamazio@gmail.com>
2023-12-07 19:39:27 -0300

    MovieAPI

---
+++ MoviesAPI/MoviesAPI/Controllers/MovieController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MoviesAPI.Controllers.Models;
+
+namespace MoviesAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+
+public class MovieController : ControllerBase
+{
+    private static List<Movie> movies = new List<Movie>();
+
+    [HttpPost]
+    public void AddMovie([FromBody]Movie movie)
+    {
+        if (!string.IsNullOrEmpty(movie.Title) &&
+            movie.Title.Length <= 500 &&
+            !string.IsNullOrEmpty(movie.Title) &&
+            movie.Duration >= 70)
+        {
+            movies.Add(movie);
+            Console.WriteLine(movie.Title);
+            Console.WriteLine(movie.Duration);
+
+        } 
+        
+    }
+
+}
+
