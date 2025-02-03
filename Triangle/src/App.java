48f1440d3e7f23cae5304ea1600dc9054f25dbcc
Giovane <giovxnedamazio@gmail.com>
2025-02-03 11:38:44 -0300

    trianglearea

---
+++ Triangle/src/App.java
@@ -0,0 +1,44 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        Triangle x, y;
+        x = new Triangle();
+        y = new Triangle();
+    
+        System.out.println("Enter the measures of triangle X: ");
+        x.a = sc.nextDouble();
+        x.b = sc.nextDouble();
+        x.c = sc.nextDouble();
+
+        System.out.println("Enter the measures of triangle Y: ");
+        y.a = sc.nextDouble();
+        y.b = sc.nextDouble();
+        y.c = sc.nextDouble();
+
+        double p = (x.a + x.b + x.c) / 2.0;
+        double areaX = Math.sqrt(p * (p - x.a) * (p - x.b) * (p - x.c));
+
+        p = (y.a + y.b + y.c) / 2.0;
+        double areaY = Math.sqrt(p * (p - y.a) * (p - y.b) * (p - y.c));
+
+        System.out.printf("Triangle X area: %.4f%n", areaX);
+        System.out.printf("Triangle Y area: %.4f%n", areaY);
+
+        if (areaX > areaY) {
+            System.out.println("Larger area: X");
+        } if (areaX < areaY){
+            System.out.println("Larger area: Y");
+        } else {
+            System.out.println("They have the same area.");
+        }
+
+        sc.close();
+        
+    }
+}
