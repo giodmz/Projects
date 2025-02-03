7437e6b147ea9b9e62e9513e0b5aa49f0e31076e
Giovane <giovxnedamazio@gmail.com>
2025-02-03 19:29:49 -0300

    stocksv1

---
+++ Stock/src/App.java
@@ -0,0 +1,34 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        Product product = new Product();
+        System.out.println("Enter product data");
+        System.out.print("Name: ");
+        product.name = sc.nextLine();
+        System.out.print("Price: ");
+        product.price = sc.nextDouble();
+        System.out.print("Quantity: ");
+        product.quantity = sc.nextInt();
+
+        System.out.println("\nProduct data: " + product);
+
+        System.out.println("Enter the number of products to be added in stock: ");
+        int quantity = sc.nextInt();
+        product.addProducts(quantity);
+        System.out.println("Updated data: " + product);
+
+        System.out.println("Enter the number of products to be removed from stock: ");
+        quantity = sc.nextInt();
+        product.removeProducts(quantity);
+        System.out.println("Updated data: " + product);
+
+
+        sc.close();
+    }
+}
