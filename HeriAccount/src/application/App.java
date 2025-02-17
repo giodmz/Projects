a18a2d74b176b3f0f8069a184024e3af9a996acc
Giovane <giovxnedamazio@gmail.com>
2025-02-17 16:45:09 -0300

    finalupdt

--- HeriAccount/src/application/App.java
+++ HeriAccount/src/application/App.java
@@ -20,31 +20,42 @@ public class App {
         BusinessAccount acc4 = (BusinessAccount)acc2;
         acc4.loan(100.0);
 
-
+        
         // TESTS
-        if(acc3 instanceof BusinessAccount){
-            BusinessAccount acc5 = (BusinessAccount)acc3;
-            acc5.loan(200.0);
-            System.out.println("Loan!");
-        }
+        Account x = new Account(111, "Astarion", 1000.0);
+        Account y = new SavingsAccount(777, "Shadowheart", 1000.0, 0.01);
 
-        if(acc3 instanceof SavingsAccount){
-            SavingsAccount acc5 = (SavingsAccount)acc3;
-            acc5.updateBalance();
-            System.out.println("Update!");
-        }
+        x.withdraw(50.0);
+        y.withdraw(50.0);
+        
+        System.out.println(x.getBalance());
+        System.out.println(y.getBalance());
 
-        Account acc6 = new Account(100, "Foda-se", 1000.0);
-        acc6.withdraw(200.0);
-        System.out.println(acc6.getBalance());
 
-        Account acc7 = new SavingsAccount(222, "Fodinha", 1000.00, 0.01);
-        acc7.withdraw(200.0);
-        System.out.println(acc7.getBalance());
 
-        Account acc8 = new BusinessAccount(102, "Bob Balão", 1000.00, 500.0);
-        acc8.withdraw(200.0);
-        System.out.println(acc8.getBalance());
+        // if(acc3 instanceof BusinessAccount){
+        //     BusinessAccount acc5 = (BusinessAccount)acc3;
+        //     acc5.loan(200.0);
+        //     System.out.println("Loan!");
+        // }
+
+        // if(acc3 instanceof SavingsAccount){
+        //     SavingsAccount acc5 = (SavingsAccount)acc3;
+        //     acc5.updateBalance();
+        //     System.out.println("Update!");
+        // }
+
+        // Account acc6 = new Account(100, "Foda-se", 1000.0);
+        // acc6.withdraw(200.0);
+        // System.out.println(acc6.getBalance());
+
+        // Account acc7 = new SavingsAccount(222, "Fodinha", 1000.00, 0.01);
+        // acc7.withdraw(200.0);
+        // System.out.println(acc7.getBalance());
+
+        // Account acc8 = new BusinessAccount(102, "Bob Balão", 1000.00, 500.0);
+        // acc8.withdraw(200.0);
+        // System.out.println(acc8.getBalance());
 
     }
 }
