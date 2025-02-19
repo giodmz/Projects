90c0d3ba8ea673b1b7a7df54cc5d05b8c1341b37
Giovane <giovxnedamazio@gmail.com>
2025-02-19 20:11:34 -0300

    final

--- Try/src/App.java
+++ Try/src/App.java
@@ -1,23 +1,39 @@
-import java.util.InputMismatchException;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
 
-        Scanner sc = new Scanner(System.in);
+        File file = new File("C:\\Projects\\Try\\in.txt");
+        Scanner sc = null;
 
         try {
-            String[] vect = sc.nextLine().split(" ");
-            int position = sc.nextInt();
-            System.out.println(vect[position]);
-
-        } catch (ArrayIndexOutOfBoundsException ex) {
-            System.out.println("Invalid Position! ");
-        } catch (InputMismatchException ex) {
-            System.out.println("Input error, try a number");
+            sc = new Scanner(file);
+            while (sc.hasNextLine()) {
+                System.out.println(sc.nextLine());
+            }
+        } catch (FileNotFoundException ex) {
+            System.out.println("Error opening file: " + ex.getMessage());
+        } finally {
+            if (sc != null) {
+                sc.close();
+            }
+            System.out.println("\nFinally block executed");
         }
 
-        sc.close();
+        // try {
+        //     String[] vect = sc.nextLine().split(" ");
+        //     int position = sc.nextInt();
+        //     System.out.println(vect[position]);
+
+        // } catch (ArrayIndexOutOfBoundsException ex) {
+        //     System.out.println("Invalid Position! ");
+        // } catch (InputMismatchException ex) {
+        //     System.out.println("Input error, try a number");
+        //     ex.printStackTrace();
+        // }
+
 
 
     }
