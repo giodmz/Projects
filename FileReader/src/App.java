8501cc47266b39d0b22f520d4c2b6056edeff702
Giovane <giovxnedamazio@gmail.com>
2025-02-25 19:40:37 -0300

    writer

--- FileReader/src/App.java
+++ FileReader/src/App.java
@@ -1,13 +1,15 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class App {
     public static void main(String[] args) throws Exception {
         
         
-        File file = new File("C:\\Projects\\FileReader\\lib\\file.txt");
+        // File file = new File("C:\\Projects\\FileReader\\lib\\file.txt");
 
         String path = "C:\\Projects\\FileReader\\lib\\file.txt";
 
@@ -35,6 +37,19 @@ public class App {
         catch (IOException ex) {
             System.out.println("Error: " + ex.getMessage());
         } 
+
+        String[] lines = new String[] {"123", "321", "788"};
+        String pathWriter = "C:\\Projects\\FileReader\\lib\\file2.txt";
+
+        // true faz com que acrescente ao arquivo e não recria ele
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(pathWriter, true))) {
+            for(String line : lines){
+                bw.write(line);
+                bw.newLine();
+            }
+        }   catch (IOException ex){
+            ex.printStackTrace();
+        }
 
     }
 }
