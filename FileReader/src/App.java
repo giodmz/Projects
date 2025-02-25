2ad320ca56ec775622cce797f5e1f9db2e1477f0
Giovane <giovxnedamazio@gmail.com>
2025-02-25 19:29:38 -0300

    att1

--- FileReader/src/App.java
+++ FileReader/src/App.java
@@ -1,25 +1,58 @@
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
-import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
         
+        
         File file = new File("C:\\Projects\\FileReader\\lib\\file.txt");
-        Scanner sc = null;
+
+        String path = "C:\\Projects\\FileReader\\lib\\file.txt";
+        FileReader fr = null;
+        BufferedReader br = null;
+
+
+        // Scanner sc = null;
+        // try {
+        //     sc = new Scanner(file);
+        //     while (sc.hasNextLine()){
+        //         System.out.println(sc.nextLine());
+        //     }
+        // }   catch (IOException ex){
+        //     System.out.println("Error: " + ex.getMessage());
+        // } finally {
+        //     sc.close();
+        // }
+        
         try {
-            sc = new Scanner(file);
-            while (sc.hasNextLine()){
-                System.out.println(sc.nextLine());
+            fr = new FileReader(path);
+            br = new BufferedReader(fr);
+
+            String line = br.readLine();
+
+            while(line != null){
+                System.out.println(line);
+                line = br.readLine();
             }
-        }   catch (IOException ex){
+        }
+        catch (IOException ex) {
             System.out.println("Error: " + ex.getMessage());
-        }
-        finally {
-            if ( sc != null){
-                sc.close();
+        } finally {
+            try{
+
+                if(br != null) {
+                    br.close();
+                }
+                if (fr != null) {
+                    fr.close();
+                }
             }
-        }
+            catch(IOException ex) {
+                ex.printStackTrace();
+            }
+            }
 
 
     }
