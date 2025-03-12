25827d3efc506accc1d68ac40edeccd9db6254c7
Giovane <giovxnedamazio@gmail.com>
2025-03-12 20:26:30 -0300

    leetq1

---
+++ LeetQ/src/App.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        System.out.println("Enter a number to check if it's a palindrome:");
+        boolean result = LeetQ1.isPalindrome(123);
+        System.out.println("Is the number a palindrome? " + result);
+    }
+
+}
---
+++ LeetQ/src/LeetQ1.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class LeetQ1 {
+        public static boolean isPalindrome(int x) {
+    
+            if (x < 0) {
+                return false;
+            }
+
+            int reverse = 0;
+            int xcopy = x;
+
+            while(x > 0) {
+                reverse = (reverse * 10) + (x % 10);
+                x /= 10;
+            }
+
+            return reverse == xcopy;
+            
+        }
+    
+}
