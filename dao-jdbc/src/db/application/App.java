a40b5a9e3fcff2273f48f80c9154a42a891f5b8a
Giovane <giovxnedamazio@gmail.com>
2025-04-28 18:04:01 -0300

    improvment department and seller

--- dao-jdbc/src/db/application/App.java
+++ dao-jdbc/src/db/application/App.java
@@ -1,11 +1,21 @@
 package db.application;
 
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 import db.model.entities.Department;
+import db.model.entities.Seller;
 
 public class App {
     public static void main(String[] args) throws Exception {
         
         Department obj = new Department(1, "we");
+        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+        String day = "21/03/2004";
+        Seller sell = new Seller(2, "ca", "ha", LocalDate.parse(day, fmt), 500.0,obj);
         System.out.println(obj);
+        System.out.println(sell);
     }
 }
--- dao-jdbc/src/db/model/entities/Seller.java
+++ dao-jdbc/src/db/model/entities/Seller.java
@@ -1,22 +1,28 @@
 package db.model.entities;
 
-import java.time.LocalDateTime;
+import java.io.Serializable;
+import java.time.LocalDate;
 
-public class Seller {
+public class Seller implements Serializable {
 
     private Integer id;
     private String name;
     private String email;
-    private LocalDateTime birthDate;
+    private LocalDate birthDate;
     private Double baseSalary;
 
+    private Department department;
 
-    public Seller(Integer id, String name, String email, LocalDateTime birthDate, Double baseSalary) {
+    
+
+    public Seller(Integer id, String name, String email, LocalDate birthDate, Double baseSalary,
+            Department department) {
         this.id = id;
         this.name = name;
         this.email = email;
         this.birthDate = birthDate;
         this.baseSalary = baseSalary;
+        this.department = department;
     }
 
     public Seller(){
@@ -26,43 +32,57 @@ public class Seller {
     public Integer getId() {
         return id;
     }
+
     public void setId(Integer id) {
         this.id = id;
     }
+
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public String getEmail() {
         return email;
     }
+
     public void setEmail(String email) {
         this.email = email;
     }
-    public LocalDateTime getBirthDate() {
+
+    public LocalDate getBirthDate() {
         return birthDate;
     }
-    public void setBirthDate(LocalDateTime birthDate) {
+
+    public void setBirthDate(LocalDate birthDate) {
         this.birthDate = birthDate;
     }
+
     public Double getBaseSalary() {
         return baseSalary;
     }
+
     public void setBaseSalary(Double baseSalary) {
         this.baseSalary = baseSalary;
     }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
 
     @Override
     public int hashCode() {
         final int prime = 31;
         int result = 1;
         result = prime * result + ((id == null) ? 0 : id.hashCode());
-        result = prime * result + ((name == null) ? 0 : name.hashCode());
-        result = prime * result + ((email == null) ? 0 : email.hashCode());
-        result = prime * result + ((birthDate == null) ? 0 : birthDate.hashCode());
-        result = prime * result + ((baseSalary == null) ? 0 : baseSalary.hashCode());
         return result;
     }
 
@@ -80,36 +100,15 @@ public class Seller {
                 return false;
         } else if (!id.equals(other.id))
             return false;
-        if (name == null) {
-            if (other.name != null)
-                return false;
-        } else if (!name.equals(other.name))
-            return false;
-        if (email == null) {
-            if (other.email != null)
-                return false;
-        } else if (!email.equals(other.email))
-            return false;
-        if (birthDate == null) {
-            if (other.birthDate != null)
-                return false;
-        } else if (!birthDate.equals(other.birthDate))
-            return false;
-        if (baseSalary == null) {
-            if (other.baseSalary != null)
-                return false;
-        } else if (!baseSalary.equals(other.baseSalary))
-            return false;
         return true;
     }
 
     @Override
     public String toString() {
-        return "Seller info - ID: " + id + ", name: " + name + ", email: " + email + ", birthDate: " + birthDate
-                + ", baseSalary: " + baseSalary;
+        return "Seller [id=" + id + ", name=" + name + ", email=" + email + ", birthDate=" + birthDate + ", baseSalary="
+                + baseSalary + ", department=" + department + "]";
     }
 
-    
     
 
 }
