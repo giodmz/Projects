06406548c41fa14d46008bbec90b23f4137b9699
Giovane <giovxnedamazio@gmail.com>
2025-05-08 09:16:21 -0300

    initial tests with JPA and maven, using hibernate

---
+++ JPA-1ts/src/App.java
@@ -0,0 +1,15 @@
+import dominio.Pessoa;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        Pessoa p1 = new Pessoa(1, "Carlos", "carlos@gmail.com");
+        Pessoa p2 = new Pessoa(2, "Maria", "maria@gmail.com");
+        Pessoa p3 = new Pessoa(1, "Astarion", "astarion@gmail.com");
+
+        System.out.println(p1);
+        System.out.println(p2);
+        System.out.println(p3);
+
+    }
+}
