d5c49a890a3b5692c7421462d24e316835954bd9
Giovane <giovxnedamazio@gmail.com>
2025-05-09 19:03:12 -0300

    config H2 database and JPA

--- springboot-demo/src/main/java/com/example/demo/entities/User.java
+++ springboot-demo/src/main/java/com/example/demo/entities/User.java
@@ -2,8 +2,19 @@ package com.example.demo.entities;
 
 import java.io.Serializable;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+// user é uma palavra reserva do banco de dados
+@Table(name = "tb_user")
 public class User implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
